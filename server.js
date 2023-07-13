2d27ad9834832cfd098b0d6ebb155baed30b7a10
darkohedgehog <zivic.darko79@gmail.com>
2023-07-13 22:55:29 +0200

    Update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : '',
     user : 'postgres',
     password : '',
     database : 'smart-brain'
